c83d5ce9d469ef27e4e9367819be091e9402c586
gelos21 <gelos21@gmail.com>
2022-11-28 15:44:24 +0000

    primer commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def suma(a: int, b: int) -> int:
+    """Permite sumar 2 números enteros.
+    
+    Args:
+        a (int): N1
+        b (int): N2
+        
+    Returns:
+        int: La suma de a + b.
+    """
+
+    return a + b
---
+++ pruebas.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+lista = [10, 20, 30, 40, 50, 60]
+a = np.array (lista)
+print(a)
